7d09aaa38233d7596a8997dd9fdbc4ca598dfd7c
SolarXYZ monke <liamvanalphen58@gmail.com>
2023-12-08 11:41:54 +0000

    Alpha 0.1

---
+++ server/database.js
@@ -0,0 +1,6 @@
+const { QuickDB } = require("quick.db");
+const Node1 = new QuickDB();
+
+(async () => {
+    await db.set("M92T36", { name: "Nintendo switch Power Charging Control IC Chip" });
+})()
