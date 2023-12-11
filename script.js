bbe8638e4778c7d414348ecbd5032b5cfb119885
SolarXYZ monke <liamvanalphen58@gmail.com>
2023-12-11 12:37:59 +0000

    Deleted Chip database made weather app

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', () => {
+    const locationElement = document.getElementById('location');
+    const temperatureElement = document.getElementById('temperature');
+    const conditionElement = document.getElementById('condition');
+    const searchForm = document.getElementById('search-form');
+    const searchInput = document.getElementById('search-input');
+  
+    // Replace 'YOUR_API_KEY' with your actual API key
+    const apiKey = '19d8092f0e094ca380d123019231112';
+    let apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=New+York`;
+  
+    function fetchWeatherData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          locationElement.textContent = data.location.name;
+          temperatureElement.textContent = data.current.temp_c + '°C';
+          conditionElement.textContent = data.current.condition.text;
+        })
+        .catch(error => console.log(error));
+    }
+  
+    searchForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const searchQuery = searchInput.value;
+      apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchQuery}`;
+      fetchWeatherData(apiUrl);
+      searchInput.value = '';
+    });
+  
+    // Fetch weather data for the default location
+    fetchWeatherData(apiUrl);
+  });
--- server/database.js
+++
@@ -1,6 +0,0 @@
-const { QuickDB } = require("quick.db");
-const Node1 = new QuickDB();
-
-(async () => {
-    await db.set("M92T36", { name: "Nintendo switch Power Charging Control IC Chip" });
-})()
